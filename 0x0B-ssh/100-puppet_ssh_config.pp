fe0c8069755757fadfb13b04529fc5801bf300da
janemumbi730 <janemumbi730@gmail.com>
2023-09-24 03:37:42 -0700

    advanced

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Ensure SSH client configuration file exists
+
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'No Authenticate with passowrd':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
