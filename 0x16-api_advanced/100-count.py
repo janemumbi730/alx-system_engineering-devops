51120c36e4fb54a5df449ae15a8a43aedcaa77be
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-11-07 21:49:51 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
 
-"""Count keywords"""
+"""Count keywords for given list in reddit API"""
 import requests
 from collections import Counter
 
 
 def count_words(subreddit, word_list, after=None, counts=None):
-    """Count keywords"""
-    # Initialize counts dictionary if not provided
+    """Count keywords for given list in API reddit"""
     if counts is None:
         counts = Counter()
-
-    # Create the Reddit API URL
     base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'your-user-agent'}
-
-    # Make the API request
     response = requests.get(base_url, headers=headers, params={'after': after})
-
-    # Check for valid subreddit
     if response.status_code != 200:
         if response.status_code == 302:
             print("Invalid subreddit or no posts found.")
         return
-
-    # Parse the JSON response
     data = response.json()
-
-    # Extract and count words in the titles
     for post in data['data']['children']:
         title = post['data']['title'].lower()
         for word in word_list:
             if title.count(word.lower()) > 0:
                 counts[word.lower()] += title.count(word.lower())
-
-    # Check for more pages
     if data['data']['after'] is not None:
         count_words(subreddit, word_list, data['data']['after'], counts)
     else:
@@ -45,6 +32,7 @@ def count_words(subreddit, word_list, after=None, counts=None):
 
 if __name__ == '__main__':
     import sys
+
 
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
