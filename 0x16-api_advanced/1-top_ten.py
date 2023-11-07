5470d4e635312d09287f14649f5de8420c20ea62
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-11-07 09:36:10 +0300

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Gets 10 hot posts for subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Query number of subscribers in subreddit"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Custom"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        hotposts = data['data']['children']
+
+
+        for child in hotposts:
+            print(child['data']['title'])
+
+    else:
+        print("None")
