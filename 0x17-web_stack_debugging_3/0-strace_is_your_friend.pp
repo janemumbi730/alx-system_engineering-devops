968ff3f5ad192bb150b00594ea6c68bd6a83cd7f
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-11-07 09:24:35 +0300

    pp file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# config files to load php
+exec {'replacing_path':
+  command => 'sed -i "s/.phpp/.php/g"  /var/www/html/wp-settings.php',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
+}
